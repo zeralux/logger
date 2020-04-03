a610a15701488a6a99c4d9ba4f3518f1d8e08c7d
david9453 <Zeralux@gmail.com>
2020-04-03 20:06:56 +0800

    增加 maven remote reposity

--- src/main/java/com/example/logger/LoggerApplication.java
+++ src/main/java/com/example/logger/LoggerApplication.java
@@ -8,11 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoggerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoggerApplication.class, args);
+//        SpringApplication.run(LoggerApplication.class, args);
 
         // 測試
-        SLF4JDemo slf4Demo = new SLF4JDemo();
-        slf4Demo.testSlf4j();
+//        SLF4JDemo slf4Demo = new SLF4JDemo();
+//        slf4Demo.testSlf4j();
+
+//        第一個問題是給你兩個字串然後回相同的部分
+        String str1 = "abcde";
+        String str2 = "ace";
+
+
+
+//        第二個問題是給你兩個數字的集合List 回一個排序好且合併的list
+
 
     }
 
